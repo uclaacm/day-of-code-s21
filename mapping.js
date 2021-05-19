4f36133ef0528e64c84f3273061f38d787f50e06
Matthew Nieva <matthewcn56@gmail.com>
2021-05-18 20:52:57 -0700

    Added Winners Carousel

--- mapping.js
+++ mapping.js
@@ -50,6 +50,7 @@ function createListOfProjects() {
       date: new Date(2021, 3, 24),
     },
   ];
+
   //sort siteNames by newest first
   //TODO: Make it support both ascending/descending date time
   siteNames.sort(
@@ -66,7 +67,7 @@ function createListOfProjects() {
     //creating anchor element
     let newProj = document.createElement("a");
     newProj.className = "project";
-    newProj.rel = "no referrer noopener";
+    newProj.rel = "noreferrer noopener";
     newProj.target = "_blank";
     newProj.href = "showcase-pages/" + proj.path + "/index.html";
 
@@ -88,4 +89,63 @@ function createListOfProjects() {
     projectsList.appendChild(newProj);
   }
   document.body.appendChild(projectsList);
+}
+
+function createListOfWinners() {
+  const winners = [
+    {
+      path: "joestar",
+      name: "The Joestar Family (part 1-5)",
+      //April 24, 2021
+      date: new Date(2021, 3, 24),
+    },
+    {
+      path: "australian",
+      name: "Australian Animals",
+      //April 24, 2021
+      date: new Date(2021, 3, 24),
+    },
+    {
+      path: "genshin",
+      name: "Our Example!",
+      //April 24, 2021
+      date: new Date(2021, 3, 24),
+    },
+    {
+      path: "kimi",
+      name: "Kimi No Na Wa",
+      //April 24, 2021
+      date: new Date(2021, 3, 24),
+    },
+  ];
+
+  //get element of the list of winners
+  let winnersList = document.getElementById("winner-projects");
+  for (let winner of winners) {
+    //creating anchor element
+    let newWinner = document.createElement("a");
+    newWinner.className = "project carousel-cell";
+
+    newWinner.rel = "noreferrer noopener";
+    newWinner.target = "_blank";
+    newWinner.href = "showcase-pages/" + winner.path + "/index.html";
+
+    //creating inner image within anchor element
+    let newWinnerImage = document.createElement("img");
+    newWinnerImage.className = "project-image";
+    newWinnerImage.src =
+      "showcase-pages/" + winner.path + "/" + winner.path + ".png";
+    newWinner.appendChild(newWinnerImage);
+
+    //creating title section within anchor
+    let newWinnerTitle = document.createElement("div");
+    newWinnerTitle.className = "project-name";
+    let newWinnerText = document.createTextNode(winner.name);
+    newWinnerTitle.appendChild(newWinnerText);
+
+    //adding title to winner
+    newWinner.appendChild(newWinnerTitle);
+    //adding new winner to list of winners
+    winnersList.appendChild(newWinner);
+  }
 }
